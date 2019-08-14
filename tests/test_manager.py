513b38da8482fea006028351999e23e4e7a20f19
Mark van Holsteijn <markvanholsteijn@binx.io>
2019-08-14 17:44:51 +0200

    formatted tests

--- tests/test_manager.py
+++ tests/test_manager.py
@@ -2,43 +2,56 @@ import boto3
 from asg_elastic_ip_manager import handler, Manager
 
 event = {
-          'detail-type': 'EC2 Instance Launch Successful',
-          'source': 'aws.autoscaling',
-          'detail': {
-            'StatusCode': 'InProgress',
-            'AutoScalingGroupName': 'asg-elastic-ip-manager-demo',
-            'Details': {
-              'Availability Zone': 'us-west-2b',
-              'Subnet ID': 'subnet-12345678'
-            },
-            'EC2InstanceId': 'i-1234567890abcdef0'
-          }
-        }
+    "detail-type": "EC2 Instance Launch Successful",
+    "source": "aws.autoscaling",
+    "detail": {
+        "StatusCode": "InProgress",
+        "AutoScalingGroupName": "asg-elastic-ip-manager-demo",
+        "Details": {"Availability Zone": "us-west-2b", "Subnet ID": "subnet-12345678"},
+        "EC2InstanceId": "i-1234567890abcdef0",
+    },
+}
 
 
-ec2 = boto3.client('ec2')
-autoscaling = boto3.client('autoscaling')
-asg = autoscaling.describe_auto_scaling_groups(AutoScalingGroupNames=[event['detail']['AutoScalingGroupName']])['AutoScalingGroups'][0]
+ec2 = boto3.client("ec2")
+autoscaling = boto3.client("autoscaling")
+asg = autoscaling.describe_auto_scaling_groups(
+    AutoScalingGroupNames=[event["detail"]["AutoScalingGroupName"]]
+)["AutoScalingGroups"][0]
+
 
 def is_available_address(a):
-    return 'AssociationId' not in a
+    return "AssociationId" not in a
+
 
 def is_unavailable_address(a):
-    return 'AssociationId' in a
+    return "AssociationId" in a
+
 
 def get_addresses():
-    response = ec2.describe_addresses(Filters=[{'Name': 'domain', 'Values': ['vpc']}, {'Name': 'tag:EIPPoolName', 'Values': ['eip-bastion-pool']}])
-    addresses = response['Addresses']
-    return addresses, list(filter(is_available_address, addresses)), list(filter(is_unavailable_address, addresses))
+    response = ec2.describe_addresses(
+        Filters=[
+            {"Name": "domain", "Values": ["vpc"]},
+            {"Name": "tag:EIPPoolName", "Values": ["eip-bastion-pool"]},
+        ]
+    )
+    addresses = response["Addresses"]
+    return (
+        addresses,
+        list(filter(is_available_address, addresses)),
+        list(filter(is_unavailable_address, addresses)),
+    )
+
 
 def return_all_ips_to_pool():
-    for instance_id in map(lambda i: i['InstanceId'], asg['Instances']):
-        event['detail-type'] = 'EC2 Instance Terminate Successful'
-        event['detail']['EC2InstanceId'] = instance_id
+    for instance_id in map(lambda i: i["InstanceId"], asg["Instances"]):
+        event["detail-type"] = "EC2 Instance Terminate Successful"
+        event["detail"]["EC2InstanceId"] = instance_id
         manager = Manager(event)
         manager.handle()
 
-def test_add_and_remove():
+
+def test_remove_and_add():
     addresses, available, allocated = get_addresses()
     assert len(addresses) == 2
 
@@ -47,16 +60,16 @@ def test_add_and_remove():
     addresses, available, _ = get_addresses()
     assert len(available) == 2
 
-    event['detail-type'] = 'EC2 Instance Launch Successful'
-    event['detail']['EC2InstanceId'] = asg['Instances'][0]['InstanceId']
+    event["detail-type"] = "EC2 Instance Launch Successful"
+    event["detail"]["EC2InstanceId"] = asg["Instances"][0]["InstanceId"]
     manager = Manager(event)
     manager.handle()
 
     addresses, available, allocated = get_addresses()
     assert len(available) == 0
 
-    event['detail-type'] = 'EC2 Instance Terminate Successful'
-    event['detail']['EC2InstanceId'] = allocated[0]['InstanceId']
+    event["detail-type"] = "EC2 Instance Terminate Successful"
+    event["detail"]["EC2InstanceId"] = allocated[0]["InstanceId"]
     manager = Manager(event)
     manager.handle()
 
@@ -64,17 +77,16 @@ def test_add_and_remove():
     assert len(available) == 1
 
 
-
 def test_remove_non_existing_instance():
-    event['detail-type'] = 'EC2 Instance Terminate Successful'
-    event['detail']['EC2InstanceId'] = 'i-0000000000000'
+    event["detail-type"] = "EC2 Instance Terminate Successful"
+    event["detail"]["EC2InstanceId"] = "i-0000000000000"
     manager = Manager(event)
     manager.handle()
+
 
 def test_add_non_existing_instance_eip():
     return_all_ips_to_pool()
-    event['detail-type'] = 'EC2 Instance Launch Successful'
-    event['detail']['EC2InstanceId'] = 'i-0000000000000'
+    event["detail-type"] = "EC2 Instance Launch Successful"
+    event["detail"]["EC2InstanceId"] = "i-0000000000000"
     manager = Manager(event)
     manager.handle()
-
